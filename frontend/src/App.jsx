74bbc0ced615efce66028b04ce1d7fced2335fe2
Priyanshu Kumar <priyanshukumarpriyanshu015@gmail.com>
2026-01-29 18:52:55 +0530

    presentation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Register from './pages/Register'
-import Login from './pages/Login'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import Routeing from "./pages/Route";
-import EcoBot from './pages/EcoBot'
+import EcoBot from "./pages/EcoBot";
+import Presentation from "./pages/Pitch";
 
 const App = () => {
   return (
     <Routes>
-
-      <Route path='/' element = {<Home />}/>
-      <Route path='/register' element = {<Register />}/>
-      <Route path='/login' element={<Login />}/>
-      <Route path='/routes' element = {<Routeing />}/>
-         <Route path="/chat" element={<EcoBot />} />
-
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/routes" element={<Routeing />} />
+      <Route path="/chat" element={<EcoBot />} />
+      <Route path="/pitch" element={<Presentation />}/>
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
